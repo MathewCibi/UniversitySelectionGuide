20920d24f947fb4f1b37822acdfee3a322ddb90e
MathewCibi <79239937+MathewCibi@users.noreply.github.com>
2023-05-15 09:15:03 +1200

    Setting up node application.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+console.log("Hello from Node!")
+
 const app = express();
 const publicPath = path.join(__dirname, 'public_html');
 
