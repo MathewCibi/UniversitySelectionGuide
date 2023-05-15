b8e7a70e1a73df1e8caf68c8c2b0e583ea35eaaf
MathewCibi <79239937+MathewCibi@users.noreply.github.com>
2023-05-16 10:14:52 +1200

    update

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const pg = require('pg')
+const cors = require('cors');
+const pg = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
 
-console.log("Loading server-side backend node script.")
+console.log("Loading server-side backend node script.");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
 
+// Post
+app.post('/insert', (request, response) => {
+
+});
+
 // Read request handler
 app.get('getAll', (request, response) => {
+    console.log('Recieved API Call')
     response.json({
         success: true
     });
-})
+});
 
-app.listen(21)
+app.listen(process.env.PORT, () => console.log('app is running'));
