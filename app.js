d294612834030d2d2b21799f1e7f24ef814a51fd
ProtonPLUS <79239937+MathewCibi@users.noreply.github.com>
2023-05-18 09:51:42 +1200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function isNumeric(str) {
 app.get('/post', (request, response) => {
     console.log('Posting!');
     try {
-        const query_text = "INSERT INTO $1 (name, opinion, accommodation, teaching, community) VALUES ($2, $3, $4, %5, $6)"
+        const query_text = "INSERT INTO $1 (name, opinion, accommodation, teaching, community) VALUES ($2, $3, $4, $5, $6)"
         // PRONE TO SQL INJECTION.
         var table = request.query.table;
         var name = request.query.name;
